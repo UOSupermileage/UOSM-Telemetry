d608054cb6a1136f2459248d5a5e76ad3db1da7d
etara <etarabey03@gmail.com>
2023-08-29 17:40:41 -0400

    Accelerometer class updated (added collect and setup).

--- lib/UOSM-Sensor/Inc/Accelerometer.hpp
+++ lib/UOSM-Sensor/Inc/Accelerometer.hpp
@@ -3,21 +3,53 @@
 //
 #include "Sensor.hpp"
 #include "ApplicationTypes.h"
+#include <SPI.h>
+#include <../Adafruit_LIS3DH/Adafruit_LIS3DH.h>
+#include <../Adafruit_Sensor/Adafruit_Sensor.h>
 
-class Accelerometer: public Sensor{
+
+#define MISO 37
+#define MOSI 35
+#define SCLK 36
+#define CS 1
+
+
+
+
+
+class Accelerometer: public Sensor<accel_t>{
 private:
     float vals[3];
 public:
 
-    Accelerometer(): Sensor(){
-
-
+    Adafruit_LIS3DH handle;
+    Accelerometer(uint8_t buffer_size): Sensor<accel_t>(buffer_size) {
+        handle = Adafruit_LIS3DH();
+        setup(&handle);
     };
-    void AccelData(uint8_t buffer){
+    void setup(Adafruit_LIS3DH& a_handle){
+        try{
+            if (! a_handle.begin(0x18)){
+                throw("Couldn't start the accelerometer.");
+            }
+            a_handle.setDataRate(LIS3DH_DATARATE_10_HZ);
+            a_handle.setRange(LIS3DH_RANGE_2_G);
+            printf("Initialization Successful!");
+        }
+
+        catch(error_t){
+            printf("Error has occured.");
+        }
+    }
+    void accelData(uint8_t buffer, Adafruit_LIS3DH& a_handle, accel_t a_struct){
         try {
 
+            a_handle.read();
+
+
+
         }
-        catch{
+        catch(error_t){
             printf("Error in saving data");
         }
     }
