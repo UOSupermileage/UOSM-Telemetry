5f64c86e9f77e18a9685c491ef455d344323df28
etarabey <115742087+etarabey@users.noreply.github.com>
2023-08-30 14:44:13 -0400

    Update Accelerometer.hpp

--- lib/UOSM-Sensor/Inc/Accelerometer.hpp
+++ lib/UOSM-Sensor/Inc/Accelerometer.hpp
@@ -3,31 +3,32 @@
 //
 #include "Sensor.hpp"
 #include "ApplicationTypes.h"
-#include <SPI.h>
-#include <../Adafruit_LIS3DH/Adafruit_LIS3DH.h>
-#include <../Adafruit_Sensor/Adafruit_Sensor.h>
+#include "../Adafruit_LIS3DH/Adafruit_LIS3DH.h"
+#include "../Adafruit_LIS3DH/Adafruit_LIS3DH.cpp"
+#include "../Adafruit_BusIO/Adafruit_BusIO_Register.h"
+#include "../Adafruit_Sensor/Adafruit_Sensor.h"
 
 
 #define MISO 37
 #define MOSI 35
 #define SCLK 36
 #define CS 1
+#define G LIS3DH_DATA
 
 
 
 
 
 class Accelerometer: public Sensor<accel_t>{
-private:
-    float vals[3];
+
 public:
 
     Adafruit_LIS3DH handle;
-    Accelerometer(uint8_t buffer_size): Sensor<accel_t>(buffer_size) {
+     Accelerometer(uint8_t buffer_size): Sensor<accel_t>(buffer_size) {
         handle = Adafruit_LIS3DH(CS,MOSI, MISO, SCLK);
-        setup(&handle);
+        setup(handle);
     };
-    void setup(Adafruit_LIS3DH& a_handle){
+    void setup(Adafruit_LIS3DH a_handle){
         try{
             if (! a_handle.begin(0x18)){
                 throw("Couldn't start the accelerometer.");
@@ -41,16 +42,22 @@ public:
             printf("Error has occured.");
         }
     }
-    void accelData(uint8_t buffer, Adafruit_LIS3DH& a_handle, accel_t a_struct){
-        try {
-
-            a_handle.read();
 
 
 
-        }
-        catch(error_t){
-            printf("Error in saving data");
-        }
+    void collect() override {
+        accel_t acceleration;
+        handle.read();
+        sensors_event_t an_event;
+        handle.getEvent(&an_event);
+        acceleration.accel_x = an_event.acceleration.x;
+        acceleration.accel_y = an_event.acceleration.y;
+        acceleration.accel_z = an_event.acceleration.z;
+        acceleration.x = handle.x;
+        acceleration.y = handle.y;
+        acceleration.z = handle.z;
+        //add(acceleration);
+
     }
+
 };
