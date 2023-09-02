37463a9685f65eae21f8b0d2c247c38ad8a487f8
etara <etarabey03@gmail.com>
2023-09-02 09:20:35 -0400

    Fixed issus receiving data with LIS3DH accelerometer and added Arduino IOoT commands.

--- lib/UOSM-Sensor/Inc/Accelerometer.hpp
+++ lib/UOSM-Sensor/Inc/Accelerometer.hpp
@@ -3,54 +3,79 @@
 //
 #include "Sensor.hpp"
 #include "ApplicationTypes.h"
-#include <SPI.h>
-#include <../Adafruit_LIS3DH/Adafruit_LIS3DH.h>
-#include <../Adafruit_Sensor/Adafruit_Sensor.h>
+#include "../Adafruit_LIS3DH/Adafruit_LIS3DH.h"
+#include "../Adafruit_LIS3DH/Adafruit_LIS3DH.cpp"
+#include "../Adafruit_BusIO/Adafruit_BusIO_Register.h"
+#include "../Adafruit_Sensor/Adafruit_Sensor.h"
+#include "../SparkFun_LIS3DH_Arduino_Library-master/SparkFun_LIS3DH_Arduino_Library-master/src_files/SparkFunLIS3DH.h"
+#include "../SparkFun_LIS3DH_Arduino_Library-master/SparkFun_LIS3DH_Arduino_Library-master/src_files/SparkFunLIS3DH.cpp" 
+#include "SPI.h"
 
 
-#define MISO 37
-#define MOSI 35
-#define SCLK 36
-#define CS 1
+#define MISO 19
+#define MOSI 23
+#define SCLK 18
+#define CS 5
+#define G 2
+#define SAMPLE_RATE 400
 
 
 
 
 
 class Accelerometer: public Sensor<accel_t>{
-private:
-    float vals[3];
+
 public:
 
-    Adafruit_LIS3DH handle;
-    Accelerometer(uint8_t buffer_size): Sensor<accel_t>(buffer_size) {
-        handle = Adafruit_LIS3DH(CS,MOSI, MISO, SCLK);
-        setup(&handle);
-    };
-    void setup(Adafruit_LIS3DH& a_handle){
+    accel_t acceleration; 
+    LIS3DH handle = LIS3DH(SPI_MODE,CS);    
+    // byte WriteData = 0; 
+    float readDatax, readDatay, readDataz = 0; 
+    explicit Accelerometer(uint8_t buffer_size): Sensor<accel_t>(buffer_size) {
+        
+        handle.begin(); 
+        handle.settings.accelRange = G; 
+        handle.settings.accelSampleRate = SAMPLE_RATE; 
+        
+      //  handle.readRegister(&readData,LIS3DH_WHO_AM_I); 
+        
+    }; 
+   /* void setup(Adafruit_LIS3DH* _a){
         try{
-            if (! a_handle.begin(0x18)){
+            if (! _a ->begin()){ 
                 throw("Couldn't start the accelerometer.");
             }
-            a_handle.setDataRate(LIS3DH_DATARATE_10_HZ);
-            a_handle.setRange(LIS3DH_RANGE_2_G);
-            printf("Initialization Successful!");
+            _a->setDataRate(LIS3DH_DATARATE_10_HZ);
+            _a->setRange(LIS3DH_RANGE_2_G);
+            Serial.println("Initialization Successful!");
         }
 
         catch(error_t){
             printf("Error has occured.");
         }
+    }*/
+
+
+
+    void collect() override {
+        Serial.printf("Bonjour");
+        readDatax = handle.readFloatAccelX(); 
+        readDatay = handle.readFloatAccelY(); 
+        readDataz = handle.readFloatAccelZ(); 
+        acceleration.accel_x = readDatax*9.81; 
+        acceleration.accel_y = readDatay*9.81; 
+        acceleration.accel_z = readDataz*9.81; 
+
+        /*handle.read();
+        handle.getEvent(&an_event);
+        acceleration.accel_x = an_event.acceleration.x;
+        acceleration.accel_y = an_event.acceleration.y;
+        acceleration.accel_z = an_event.acceleration.z;
+        acceleration.x = handle.x;
+        acceleration.y = handle.y;
+        acceleration.z = handle.z;*/
+        add(acceleration); 
+
     }
-    void accelData(uint8_t buffer, Adafruit_LIS3DH& a_handle, accel_t a_struct){
-        try {
 
-            a_handle.read();
-
-
-
-        }
-        catch(error_t){
-            printf("Error in saving data");
-        }
-    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "VoltageSensor.hpp"
-
+#include "Accelerometer.hpp"
+#include "AIotvarcollect.h" 
 #ifdef ESP32
 
 #include <Arduino.h>
@@ -8,23 +9,41 @@
 #define DEFAULT_BUFFER_SIZE 10
 
 VoltageSensor* voltageSensor;
+Accelerometer* accelSensor;
 
 PollingSensorTask<voltage_t>* voltageSensorTask;
+PollingSensorTask<accel_t>* accelSensorTask;
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(115200); 
     Serial.printf("Minimal stack size %d\n", configMINIMAL_STACK_SIZE);
-    voltageSensor = new VoltageSensor(DEFAULT_BUFFER_SIZE);
-
+    //voltageSensor = new VoltageSensor(DEFAULT_BUFFER_SIZE);
+    accelSensor = new Accelerometer(DEFAULT_BUFFER_SIZE);
+    // accelSensor->addListener([](const accel_t& newValue){ 
+        accelSensor->addListener([](const accel_t& newValue){ 
+            updateAcceleration(newValue); 
+        }); 
+    // }) 
     // TODO: Note that voltageSensor will throw an exception if collect is not called before get(). See if we can apply RAII
 
-    voltageSensorTask = new PollingSensorTask<voltage_t>(voltageSensor, 200, "T_VoltageSensor", 128 * 10, 5);
+    //voltageSensorTask = new PollingSensorTask<voltage_t>(voltageSensor, 200, "T_VoltageSensor", 12800 * 100, 5);
+    accelSensorTask = new PollingSensorTask<accel_t>(accelSensor,200,"T_AccelSensor",1280*100,5); 
+    
+    initProperties(); 
+    ArduinoCloud.begin(AIoTPreferredConnection);  
+
 }
 
 void loop() {
     Serial.print("Main Loop...\n");
-    vTaskDelay(500);
-    Serial.printf("Value %d\n", voltageSensor->get());
+    vTaskDelay(100);
+    //Serial.printf("Value %d\n", voltageSensor->get());
+    // accelSensor->collect(); 
+    // Serial.printf("Value Accelerometer X: %f\n", accelSensor->get().accel_x); 
+    // Serial.printf("Value Accelerometer Y: %f\n", accelSensor->get().accel_y); 
+    // Serial.printf("Value Accelerometer Z: %f\n", accelSensor->get().accel_z);  
+    ArduinoCloud.update(); 
+    
 }
 
 #else
@@ -32,7 +51,9 @@ void loop() {
 int main() {
     printf("Starting");
     VoltageSensor sensor(10);
+    Accelerometer sens_accel(10);
     sensor.collect();
+    sens_accel.collect(10);
     printf("Get Value %d", sensor.get());
     return 0;
 }
