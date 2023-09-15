49b68b73256a780bbf15a7c549eb3b9ba3cc22aa
Jeremy <jeremymcote@gmail.com>
2023-09-15 13:43:42 -0400

    Add documentation for mutex

--- include/Mutex.hpp
+++ include/Mutex.hpp
@@ -8,12 +8,24 @@
 #define DEFAULT_MUTEX_DELAY 50
 #define DEFAULT_MUTEX_RETRIES 6
 
-// Mutex
+/**
+ * Ensure that only one core can access a protected resource at once.
+ */
 class Mutex {
 private:
     bool locked = false;
 public:
+    /**
+     * Get the status of the mutex.
+     * @return true if the mutex's resource is in use.
+     */
     [[nodiscard]] bool getLocked() const { return locked; }
+
+    /**
+     * Lock the mutex.
+     * Use before accessing the data it protects.
+     * @return
+     */
     bool lock() {
         if (locked) {
             return false;
@@ -21,9 +33,22 @@ public:
         locked = true;
         return true;
     }
+
+    /**
+     * Unlock the mutex.
+     * Use once an operation is complete
+     */
     void unlock() {
         locked = false;
     }
+
+    /**
+     * Await mutex unlock and then execute the callback.
+     * This will defer execution to other tasks while blocked.
+     * @param callback Function to execute when the mutex is unlocked.
+     * @param retryDelay Number of milliseconds between retries
+     * @param maxTries Max number of retries before continuing without executing callback
+     */
     void execute(const std::function<void()>& callback, uint32_t retryDelay = DEFAULT_MUTEX_RETRIES, uint8_t maxTries = DEFAULT_MUTEX_RETRIES) {
         uint8_t tries = 0;
         while (tries < maxTries) {
